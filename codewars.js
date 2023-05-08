605eae10434af407b4390da7876e25bab6d569f0
Vaughn Nedderman <vaughn@nedderman.com>
2023-05-08 19:46:35 -0400

    problem 2

--- codewars.js
+++ codewars.js
@@ -2,18 +2,24 @@
 // Testing 1-2-3
 // https://www.codewars.com/kata/54bf85e3d5b56c7a05000cf9/train/javascript
 
+var number = function (array) {
+  //   return array.map((x, i)  => (i + 1) + ": " + x);
+  return array.map(function (x, i) {
+    return i + 1 + ": " + x;
+  });
+};
+
 // # 2
 // Filling an array (part 1)
 // https://www.codewars.com/kata/571d42206414b103dc0006a1/train/javascript
 
-const arr = (N) => [0, 1, 2, 3 /* the numbers 0 to N-1 */];
-for (let i = 0; i < arr.length; i++) {
-  if ((arr.length = 0)) {
-    return (arr = []);
-  } else {
-    return arr[i];
+const arr = (N) => {
+  let array = [];
+  for (i = 0; i < N; i++) {
+    array.push(i);
   }
-}
+  return array;
+};
 
 // # 3
 // Drone Fly-By
